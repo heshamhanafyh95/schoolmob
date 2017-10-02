4f29caf8e98bb1055b49f2a97998c7ff50a5734d
Nourkhallaf <nkhallaf94@gmail.com>
2017-10-02 22:05:26 +0200

    2nd

--- app/src/main/java/com/schoolpua/schoolmob/MainActivity.java
+++ app/src/main/java/com/schoolpua/schoolmob/MainActivity.java
@@ -1,19 +1,32 @@
 package com.schoolpua.schoolmob;
 
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.*;
+//import com.google.firebase.auth.AuthResult;
+//import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.*;
+
 
 public class MainActivity extends AppCompatActivity {
-    Button btn1;
+    private Button btnlog;
+    private EditText inputEmail, inputPassword;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        btn1=(Button)findViewById(R.id.btn1);
-        btn1.setOnClickListener(new View.OnClickListener() {
+        inputEmail=(EditText)findViewById(R.id.email);
+        inputPassword=(EditText)findViewById(R.id.password);
+        btnlog=(Button)findViewById(R.id.btn_login);
+
+        btnlog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,home.class));
